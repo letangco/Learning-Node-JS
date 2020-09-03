0f3cccaecde8a30dbbf7e3f3aa3777630ef62064
TangCo <17520296@gm.uit.edu.vn>
2020-09-03 09:17:53 +0700

    Template Engines and use pug

--- Bai2-TemplateEngine/index.js
+++ Bai2-TemplateEngine/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views','./views');
+
 const port =3000;
 app.get('/',function(request, response){
-    response.send("<h1>Hello Node JS</h1>");
+    response.render("index",{
+        name: "Le Tang Co"
+    });
+});
+app.get('/user',function(request, response){
+    response.render('user', { title: 'Hey', message: 'Hello there!' ,users:[{id:1,name: 'A'},{id:2,name:'B'}]})
 });
 app.listen(port,()=>{
     console.log("server app listening at port"+ port);
