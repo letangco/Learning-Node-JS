39d1da0d469a3db1cef5285578ca97dc49dbc291
TangCo <17520296@gm.uit.edu.vn>
2020-09-10 14:44:16 +0700

    Debug Node Js App

---
+++ Bai18- DebugNodeJS/middleware/dist/auth.middleware.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var db = require('../db');
+
+module.exports.requireAuth = function (req, res, next) {
+  if (!req.cookies.userID) {
+    res.redirect('/auth/login');
+    return;
+  }
+
+  var user = db.get('users').find({
+    id: req.cookies.userID
+  }).value();
+
+  if (!user) {
+    res.redirect('/auth/login');
+    return;
+  }
+
+  next();
+};
