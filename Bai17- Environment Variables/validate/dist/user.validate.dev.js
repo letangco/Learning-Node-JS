615468bde11aeca14d911ab4f79dc8cdb7061b5d
TangCo <17520296@gm.uit.edu.vn>
2020-09-10 14:22:32 +0700

    Tham số truyền Cookie được lưu bởi dotenv

---
+++ Bai17- Environment Variables/validate/dist/user.validate.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports.postAdd = function (req, res, next) {
+  var error = [];
+
+  if (!req.body.name) {
+    error.push("Name is required!");
+  }
+
+  if (!req.body.phone) {
+    error.push("Phone is required!");
+  }
+
+  if (error.length) {
+    res.render('add', {
+      errors: error,
+      values: req.body
+    });
+    return;
+  }
+
+  res.locals.success = true;
+  next();
+};
